25d82aee7b4d0b48e7c1eb04e805b52051733df1
santosvanessa <vanessa.explore1d@gmail.com>
2017-07-09 19:04:11 -0300

    será?

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -19,17 +19,14 @@ const HEROES : Hero[] = [
 @Component({
   selector: 'hero-detail',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']})
-
+  styleUrls: ['./app.component.css'
+]})
   export class AppComponent {
   title = 'Tour of Heroes';
   selectedHero : Hero;
   heroes = HEROES;
-
+  @Input() hero: Hero;
   onSelect(hero: Hero) : void {
       this.selectedHero = hero;
   }
-}
-export class HeroDetailComponent {
-    @Input() hero: Hero;
 }
